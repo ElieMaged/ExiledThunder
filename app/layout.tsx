50bc2f483a25a8248958ca68462029de0443e6f0
ElieMaged <Narutossj23@yahoo.com>
2024-03-21 17:35:46 +0200

    almost done!

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.css"
+import "./app.css"
+import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ["latin"] });
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,12 +15,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{children: React.ReactNode;}>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <div>
+        <Navbar />
+      </div>
+            <body className={inter.className}>{children}</body>
+            </>
   );
 }
