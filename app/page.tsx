acb816dc70c09f5cfe7f0dd4a342117628e55980
ElieMaged <Narutossj23@yahoo.com>
2024-08-26 22:39:10 +0300

    q

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,16 @@ import React from "react"
 export default function Home() {
   return (
     <>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 mainPage">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">    
+    <main className="container flex flex-col p-12 text-center space-y-20">
+      <div className="font-mono ">    
            </div>
       <div className="">
-                 <h1 className="text-5xl">
-          Where Indie Dreams Take Flight!   
+      <h1 className="text-5xl mb-12">
+          Where Indie Dreams Take Flight!
       </h1>
+      <p>
+        We create games that spark the imagination and reignite the fire in your heart!
+      </p>
            </div>
       <div className="mb-32 grid text-center lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-3 lg:text-left ">   
 
